57cf9fac41f48ae78f73cafc9d4b143964c0165f
Arjun Choudhary <68503832+Arjun72757@users.noreply.github.com>
2026-01-24 23:51:38 +0530

    Add files via upload

---
+++ WhileExample.java
@@ -0,0 +1,21 @@
+package LOOP;
+
+import java.util.Scanner;
+
+public class WhileExample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int i = 1;
+        while (i <= n) {
+            System.out.println(i);
+            i++;
+        }
+
+        sc.close();
+    }
+}
+
