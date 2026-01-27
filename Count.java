2835f4ba40950374d405deef9540a026a0107809
Arjun Choudhary <68503832+Arjun72757@users.noreply.github.com>
2026-01-27 23:28:48 +0530

    Add files via upload

---
+++ Count.java
@@ -0,0 +1,19 @@
+package LOOP;
+
+import java.util.Scanner;
+
+public class Count {
+    public static void  main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n=sc.nextInt();
+        int count = 0;
+        while(n!=0){
+            n=n/10;
+            count++;
+        }
+        System.out.println("Count digit is  " + count);
+        sc.close();
+
+    }
+}
